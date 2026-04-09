5fb9ca4db2d2301620803a717290ed01e3d72800
Jhonskiee <166783857+Jhonskiee@users.noreply.github.com>
2026-04-10 01:34:50 +0800

    script.js

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 window.onload = function() {
-  firebase.initializeApp(firebaseConfig);
+ const firebaseConfig = {
+    apiKey: "AIzaSyA4F9GnvJZ-AGzID63vqQO79zJulh2NJmY",
+    authDomain: "abcs-5859c.firebaseapp.com",
+    databaseURL: "https://abcs-5859c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "abcs-5859c",
+    storageBucket: "abcs-5859c.firebasestorage.app",
+    messagingSenderId: "479969126573",
+    appId: "1:479969126573:web:26bdd9ff755a92615afe24",
+    measurementId: "G-KTV3112JXN"
+  };
+   firebase.initializeApp(firebaseConfig);
   var db = firebase.database();
 
   class MEME_CHAT{
