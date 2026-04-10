b3227e7ede37ba0f026e0101d36f2a5e249364e4
Jhonskiee <166783857+Jhonskiee@users.noreply.github.com>
2026-04-10 21:13:01 +0800

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 window.onload = function () {
 
   const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT.firebaseio.com",
-    projectId: "YOUR_PROJECT",
-  };
+  apiKey: "AIzaSyA4F9GnvJZ-AGzID63vqQO79zJulh2NJmY",
+  authDomain: "abcs-5859c.firebaseapp.com",
+  databaseURL: "https://abcs-5859c-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "abcs-5859c",
+  storageBucket: "abcs-5859c.firebasestorage.app",
+  messagingSenderId: "479969126573",
+  appId: "1:479969126573:web:26bdd9ff755a92615afe24",
+  measurementId: "G-KTV3112JXN"
+};
 
   firebase.initializeApp(firebaseConfig);
 
