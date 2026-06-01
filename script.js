93cdf1c9b44e24c656f06c36da5e429c04d1774e
Jhonskiee <166783857+Jhonskiee@users.noreply.github.com>
2026-06-01 14:17:09 +0800

    Script.js

--- script.js
+++ script.js
@@ -1,184 +1,63 @@
-window.onload = function () {
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-  const firebaseConfig = {
-  apiKey: "AIzaSyA4F9GnvJZ-AGzID63vqQO79zJulh2NJmY",
-  authDomain: "abcs-5859c.firebaseapp.com",
-  databaseURL: "https://abcs-5859c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "abcs-5859c",
-  storageBucket: "abcs-5859c.firebasestorage.app",
-  messagingSenderId: "479969126573",
-  appId: "1:479969126573:web:26bdd9ff755a92615afe24",
-  measurementId: "G-KTV3112JXN"
+import {
+getAuth,
+createUserWithEmailAndPassword,
+signInWithEmailAndPassword,
+signOut,
+onAuthStateChanged
+}
+from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4F9GnvJZ-AGzID63vqQO79zJulh2NJmY",
+    authDomain: "abcs-5859c.firebaseapp.com",
+    databaseURL: "https://abcs-5859c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "abcs-5859c",
+    storageBucket: "abcs-5859c.firebasestorage.app",
+    messagingSenderId: "479969126573",
+    appId: "1:479969126573:web:26bdd9ff755a92615afe24",
+    measurementId: "G-KTV3112JXN"
+  };
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+window.register = function () {
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
+
+createUserWithEmailAndPassword(auth, email, password)
+.then(() => {
+alert("Account Created!");
+})
+.catch(error => {
+alert(error.message);
+});
 };
 
-  firebase.initializeApp(firebaseConfig);
+window.login = function () {
+const email = document.getElementById("email").value;
+const password = document.getElementById("password").value;
 
-  const db = firebase.database();
-  const auth = firebase.auth();
-
-  class App {
-
-    title() {
-      let div = document.createElement('div');
-      div.id = 'title_container';
-
-      let h1 = document.createElement('h1');
-      h1.id = 'title';
-      h1.textContent = 'Life Is Unfair';
-
-      div.appendChild(h1);
-      document.body.appendChild(div);
-    }
-
-    home() {
-      document.body.innerHTML = '';
-      this.title();
-
-      let container = document.createElement('div');
-      container.className = 'container';
-
-      let name = document.createElement('input');
-      name.placeholder = 'Name';
-
-      let email = document.createElement('input');
-      email.placeholder = 'Email';
-
-      let pass = document.createElement('input');
-      pass.placeholder = 'Password';
-      pass.type = 'password';
-
-      let login = document.createElement('button');
-      login.textContent = 'Login';
-
-      let register = document.createElement('button');
-      register.textContent = 'Create Account';
-
-      let forgot = document.createElement('button');
-      forgot.textContent = 'Forgot Password';
-
-      login.onclick = () => {
-        auth.signInWithEmailAndPassword(email.value, pass.value)
-        .catch(e => alert(e.message));
-      };
-
-      register.onclick = () => {
-        auth.createUserWithEmailAndPassword(email.value, pass.value)
-        .then(user => {
-          user.user.updateProfile({ displayName: name.value });
-        })
-        .catch(e => alert(e.message));
-      };
-
-      forgot.onclick = () => {
-        if (!email.value) return alert("Enter email first");
-        auth.sendPasswordResetEmail(email.value)
-        .then(() => alert("Email sent"))
-        .catch(e => alert(e.message));
-      };
-
-      container.append(name, email, pass, login, register, forgot);
-      document.body.appendChild(container);
-    }
-
-    chat(user) {
-      document.body.innerHTML = '';
-      this.title();
-
-      let chat = document.createElement('div');
-      chat.className = 'chat';
-
-      let messages = document.createElement('div');
-      messages.className = 'messages';
-
-      let input = document.createElement('input');
-      input.placeholder = 'Type message...';
-
-      let send = document.createElement('button');
-      send.textContent = 'Send';
-
-      let typing = document.createElement('div');
-      typing.className = 'typing';
-
-      let online = document.createElement('div');
-      online.className = 'online';
-
-      let logout = document.createElement('button');
-      logout.textContent = 'Logout';
-
-      // SEND
-      send.onclick = () => {
-        if (!input.value.trim()) return;
-
-        db.ref('messages').push({
-          name: user.displayName || user.email,
-          text: input.value
-        });
-
-        db.ref('typing/' + user.uid).remove();
-        input.value = '';
-      };
-
-      // TYPING
-      input.oninput = () => {
-        db.ref('typing/' + user.uid).set(user.displayName || user.email);
-
-        setTimeout(() => {
-          db.ref('typing/' + user.uid).remove();
-        }, 2000);
-      };
-
-      // LOGOUT
-      logout.onclick = () => {
-        db.ref('online/' + user.uid).remove();
-        auth.signOut();
-      };
-
-      chat.append(messages, input, send, typing, online, logout);
-      document.body.appendChild(chat);
-
-      // REALTIME CHAT
-      db.ref('messages').on('value', snap => {
-        messages.innerHTML = '';
-        snap.forEach(d => {
-          let m = d.val();
-          let p = document.createElement('div');
-          p.className = 'message';
-          p.textContent = m.name + ": " + m.text;
-          messages.appendChild(p);
-        });
-      });
-
-      // ONLINE USERS
-      db.ref('online/' + user.uid).set(user.displayName || user.email);
-
-      db.ref('online').on('value', snap => {
-        online.innerHTML = "<b>Online:</b><br>";
-        snap.forEach(u => {
-          let p = document.createElement('div');
-          p.textContent = "🟢 " + u.val();
-          online.appendChild(p);
-        });
-      });
-
-      // TYPING DISPLAY
-      db.ref('typing').on('value', snap => {
-        typing.innerHTML = '';
-        snap.forEach(t => {
-          if (t.key !== user.uid) {
-            typing.textContent = t.val() + " is typing...";
-          }
-        });
-      });
-    }
-  }
-
-  const app = new App();
-
-  auth.onAuthStateChanged(user => {
-    if (user) {
-      app.chat(user);
-    } else {
-      app.home();
-    }
-  });
+signInWithEmailAndPassword(auth, email, password)
+.catch(error => {
+alert(error.message);
+});
 };
+
+window.logout = function () {
+signOut(auth);
+};
+
+onAuthStateChanged(auth, user => {
+if(user){
+document.getElementById("loginPage").style.display="none";
+document.getElementById("homePage").style.display="block";
+document.getElementById("userEmail").textContent=user.email;
+}
+else{
+document.getElementById("loginPage").style.display="flex";
+document.getElementById("homePage").style.display="none";
+}
+});
